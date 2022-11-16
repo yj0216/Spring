15313d721d477fa86b44bc80762fc2fef81af214
yj0216 <yeojun0216@naver.com>
2022-11-16 13:58:45 +0900

    Spring DB connection2

--- src/main/java/com/example/helloworld/repository/JdbcMemberRepository.java
+++ src/main/java/com/example/helloworld/repository/JdbcMemberRepository.java
@@ -25,7 +25,9 @@ public class JdbcMemberRepository implements MemberRepository {
             conn = getConnection();
             pstmt = conn.prepareStatement(sql,
                     Statement.RETURN_GENERATED_KEYS);
+
             pstmt.setString(1, member.getName());
+
             pstmt.executeUpdate();
             rs = pstmt.getGeneratedKeys();
             if (rs.next()) {
@@ -114,7 +116,7 @@ public class JdbcMemberRepository implements MemberRepository {
         }
     }
     private Connection getConnection() {
-        return DataSource.getConnection(dataSource);
+        return DataSourceUtils.getConnection(dataSource);
     }
     private void close(Connection conn, PreparedStatement pstmt, ResultSet rs)
     {
